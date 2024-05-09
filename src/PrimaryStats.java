5c115940376b9db45302d895c7f24c9643baf318
MhaaHabib <cph-mh1182@cphbusiness.dk>
2024-05-09 10:51:00 +0200

    lavet atributter og constructer

---
+++ src/PrimaryStats.java
@@ -0,0 +1,26 @@
+public class primaryStats {
+    protected Stat agility;
+    protected Stat constitution;
+    protected Stat empathy;
+    protected Stat intuition;
+    protected Stat memory;
+    protected Stat presence;
+    protected Stat quickness;
+    protected Stat reasoning;
+    protected Stat selfDiscipline;
+    protected Stat strength;
+
+    public primaryStats(){
+        this.agility= new Stat("agility");
+        this.constitution = new Stat("contitition");
+        this.empathy= new Stat ("empathy");
+        this.intuition= new Stat("intuition");
+        this.memory= new Stat("memory");
+        this.presence=new Stat("presence");
+        this.quickness= new Stat("quickness");
+        this.reasoning = new Stat("reasoning");
+        this.selfDiscipline= new Stat("selfDiscipline");
+        this.strength= new Stat("strength");
+    }
+
+}
