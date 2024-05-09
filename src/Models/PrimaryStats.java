6e2ce792e4d30f151fb74f7e48fd22f78d5a0bcd
AndyTheDragon <andre.jansam@gmail.com>
2024-05-09 11:23:59 +0200

    Flyttet PrimaryStats ind i paken og rettet stavefejl

--- src/Models/PrimaryStats.java
+++ src/Models/PrimaryStats.java
@@ -1,4 +1,28 @@
 package Models;
 
 public class PrimaryStats {
+    protected Stat agility;
+    protected Stat constitution;
+    protected Stat empathy;
+    protected Stat intuition;
+    protected Stat memory;
+    protected Stat presence;
+    protected Stat quickness;
+    protected Stat reasoning;
+    protected Stat selfDiscipline;
+    protected Stat strength;
+
+    public PrimaryStats(){
+        this.agility= new Stat("agility");
+        this.constitution = new Stat("contitition");
+        this.empathy= new Stat ("empathy");
+        this.intuition= new Stat("intuition");
+        this.memory= new Stat("memory");
+        this.presence=new Stat("presence");
+        this.quickness= new Stat("quickness");
+        this.reasoning = new Stat("reasoning");
+        this.selfDiscipline= new Stat("selfDiscipline");
+        this.strength= new Stat("strength");
+    }
+
 }
