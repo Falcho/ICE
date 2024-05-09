0a6772168e10050940ecad7e60a3e42b841538c5
MojoOno <Ffranck0406@gmail.com>
2024-05-09 13:52:46 +0200

    Rettede Dice klassen, fra at være lavet så hurtigt som muligt, til at være gennemtænkt en anelse mere. Dvs. fjernede static fra roll metoderne, fjernede duplicate klasse og instantierede vores Random i klassen, istedet for i metoderne.

--- src/Dice.java
+++ src/Dice.java
@@ -1,45 +1,35 @@
 import java.util.Random;
 
 public class Dice {
+    Random random = new Random();
 
-
-    public class DiceRoll {
-
-
-        public static int rollD4() {
-            Random random = new Random();
-            return random.nextInt(4) + 1;
-        }
-
-        public static int rollD6() {
-            Random random = new Random();
-            return random.nextInt(6) + 1;
-        }
-
-        public static int rollD8() {
-            Random random = new Random();
-            return random.nextInt(8) + 1;
-        }
-
-        public static int rollD10() {
-            Random random = new Random();
-            return random.nextInt(10) + 1;
-        }
-
-        public static int rollD12() {
-            Random random = new Random();
-            return random.nextInt(12) + 1;
-        }
-
-        public static int rollD20() {
-            Random random = new Random();
-            return random.nextInt(20) + 1;
-        }
-
-        public static int rollD100() {
-            Random random = new Random();
-            return random.nextInt(100) + 1;
-        }
+    public int rollD4() {
+        return random.nextInt(4) + 1;
     }
 
+    public int rollD6() {
+        return random.nextInt(6) + 1;
+    }
+
+    public int rollD8() {
+        return random.nextInt(8) + 1;
+    }
+
+    public int rollD10() {
+        return random.nextInt(10) + 1;
+    }
+
+    public int rollD12() {
+        return random.nextInt(12) + 1;
+    }
+
+    public int rollD20() {
+        return random.nextInt(20) + 1;
+    }
+
+    public int rollD100() {
+        return random.nextInt(100) + 1;
+    }
 }
+
+
