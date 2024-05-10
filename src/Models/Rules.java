4f0f246d449f21d03bc357ded868bf6710f2074e
AndyTheDragon <andre.jansam@gmail.com>
2024-05-10 09:30:03 +0200

    Oprettet Rules hjælpeklasse

---
+++ src/Models/Rules.java
@@ -0,0 +1,12 @@
+package Models;
+
+import java.util.TreeMap;
+import java.util.Map;
+
+public class Rules {
+    public Map<String, Skill> getEmptySkillList() {
+        Map<String, Skill> emptySkillList = new TreeMap<>();
+
+        return emptySkillList;
+    }
+}
