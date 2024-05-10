6371d10e34c5e8fd7ecdeefb72ba8185fa574feb
AndyTheDragon <andre.jansam@gmail.com>
2024-05-10 10:33:47 +0200

    test af charactersheet klassen

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,8 @@
+import Models.CharacterSheet;
+
 public class Main {
     public static void main(String[] args) {
+        CharacterSheet sheet = new CharacterSheet();
+        System.out.println(sheet);
     }
 }
