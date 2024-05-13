d05170d98cbc1d7e819f31faf15a3460b0667c39
Falcho <45388271+Falcho@users.noreply.github.com>
2024-05-13 11:03:21 +0200

    Revert "High Elf data"

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,12 @@
 import Models.CharacterSheet;
+import Views.MainFrame;
+
+import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
         CharacterSheet sheet = new CharacterSheet();
         System.out.println(sheet);
+        SwingUtilities.invokeLater(MainFrame::new);
     }
 }
