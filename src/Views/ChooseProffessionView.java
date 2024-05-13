a32b53d540aa07adbb01a9fadff442d952becca1
Falcho <m.k.falcham@gmail.com>
2024-05-13 10:27:56 +0200

    Added more code to class

--- src/Views/ChooseProffessionView.java
+++ src/Views/ChooseProffessionView.java
@@ -1,6 +1,10 @@
 package Views;
 
+import Models.Profession;
+import Models.SkillCategory;
+
 import javax.swing.*;
+import java.awt.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,8 +14,8 @@ public class ChooseProffessionView extends JFrame {
     private JLabel title;
     private JLabel proffessionalBonus;
     private JLabel knack;
-    private List<String> proffessionList;
-    private List<JList> skillList;
+    private JList<Profession> proffessionList;
+    private List<SkillCategory> skillList;
     private List<JList> proffessionSkillCost;
     private JTextArea proffessionDescription;
     private JCheckBox ProffessionalAndKnack;
@@ -25,14 +29,22 @@ public class ChooseProffessionView extends JFrame {
         title = new JLabel("Choose Proffession");
         proffessionalBonus = new JLabel("Bonus");
         knack = new JLabel("Knack");
-        proffessionList = new ArrayList<String>();
+        proffessionList = new JList<>();
         skillList = new ArrayList<>();
         proffessionSkillCost = new ArrayList<>();
         proffessionDescription = new JTextArea();
         ProffessionalAndKnack = new JCheckBox();
 
+        returnToCharacterName = new JButton("Return to Character Name");
+        saveAndContinueToRace = new JButton("Save and Continue to Race");
+
+        this.setLayout(new BorderLayout());
+
+        }
+
+
     }
 
 
 
-}
+
