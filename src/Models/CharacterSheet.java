281e311d360098b797fd411920fdf9b001283229
MhaaHabib <cph-mh1182@cphbusiness.dk>
2024-05-14 14:24:04 +0200

    lavet setter med en liiste i parameteren i CharacterSheet klassen

--- src/Models/CharacterSheet.java
+++ src/Models/CharacterSheet.java
@@ -1,5 +1,6 @@
 package Models;
 
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
@@ -56,6 +57,9 @@ public class CharacterSheet {
 
     public void setStat(String statName,int[]values){
         this.stats.setStat(statName,values);
+    }
 
+    public void setStats(List<int[]>liste){
+        this.stats.setStats(liste);
     }
 }
