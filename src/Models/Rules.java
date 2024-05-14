8a263271eb2ffe70135416787f6ae50afb859a69
AndyTheDragon <andre.jansam@gmail.com>
2024-05-14 12:25:02 +0200

    Startet TalentsAndFlaws in Rules

--- src/Models/Rules.java
+++ src/Models/Rules.java
@@ -1,12 +1,16 @@
 package Models;
 
-import java.util.TreeMap;
-import java.util.Map;
+import java.util.*;
 
 public class Rules {
     public static Map<String, SkillCategory> getEmptySkillList() {
         Map<String, SkillCategory> emptySkillList = new TreeMap<>();
         emptySkillList.put("Delving", new SkillCategory("Delving") );
         return emptySkillList;
+    }
+
+    public static Map<String, String> getTalentsAndFlaws() {
+        Map<String,String> talentsAndFlaws = new HashMap<>();
+        return talentsAndFlaws;
     }
 }
