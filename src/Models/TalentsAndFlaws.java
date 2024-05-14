896354f317618fa4096376d37d118cedc3354994
AndyTheDragon <andre.jansam@gmail.com>
2024-05-14 12:39:04 +0200

    Opretter TalentsAndFlaws modellen

--- src/Models/TalentsAndFlaws.java
+++ src/Models/TalentsAndFlaws.java
@@ -1,4 +1,19 @@
 package Models;
 
 public class TalentsAndFlaws {
+    private final String name;
+    private final String description;
+
+    public TalentsAndFlaws(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
 }
