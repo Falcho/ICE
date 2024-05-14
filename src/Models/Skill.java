c2e9f06645411bd812255ef0f366cc30ee413312
Falcho <m.k.falcham@gmail.com>
2024-05-14 10:13:25 +0200

    Tilføjet Attributer, og getter/setter til Skill Class, som jeg skal bruge i ChooseProffessionView

--- src/Models/Skill.java
+++ src/Models/Skill.java
@@ -1,4 +1,17 @@
 package Models;
 
 public class Skill {
+    String name;
+    int firstCost;
+    int secondCost;
+
+    public Skill() {}
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
