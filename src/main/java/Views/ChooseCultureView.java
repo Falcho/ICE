1e5d38ff43febd1c58a9962a2eab04f7ed23ca7b
AndyTheDragon <andre.jansam@gmail.com>
2024-05-15 13:15:36 +0200

    Gjort klar til data fra json

--- src/main/java/Views/ChooseCultureView.java
+++ src/main/java/Views/ChooseCultureView.java
@@ -1,10 +1,14 @@
 package Views;
 
+import Models.Culture;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class ChooseCultureView extends JPanel {
 
@@ -13,66 +17,19 @@ public class ChooseCultureView extends JPanel {
     private JTextArea description;
     private List<JTextField[]> field;
     private JComboBox<String> chooseCultureDropdown;
-
-    private JButton goToStats;
-    private JButton previous;
+    private Map<String,Culture> cultures;
+    private JButton continueButton;
+    private JButton previousButton;
 
     public ChooseCultureView() {
-        //Initialize all components
-        title = new JLabel("It's time to choose your Culture");
-        Font font = new Font(title.getFont().getName(), Font.PLAIN, 16);
-        title.setFont(font);
-        labels = new ArrayList<>();
-        field = new ArrayList<>();
-        labels.add(new JLabel("Animal Handling"));
-        labels.add(new JLabel("Riding"));
-        labels.add(new JLabel("Unarmed"));
-        labels.add(new JLabel("Melee Weapons"));
-        labels.add(new JLabel("Ranged Weapons"));
-        labels.add(new JLabel("Languages"));
-        labels.add(new JLabel("Other Lores"));
-        labels.add(new JLabel("Influence"));
-        for(int i = 0; i < labels.size(); i++){
-            JTextField[] rankField = new JTextField[1];
-            rankField[0] = new JTextField(3);
-            field.add(rankField);
-        }
+        //this.cultures = cultures;
+        cultures = new HashMap<>();
+        cultures.put("Cosmopolitan", new Culture("Cosmopolitan"));
+        cultures.put("Harsh", new Culture("Harsh"));
+        cultures.put("Highland", new Culture("Highland"));
+        cultures.put("Malta", new Culture("Malta"));
 
-
-
-
-        chooseCultureDropdown = new JComboBox<>(new String[]{"Cosmopolitan", "Harsh", "Highland", "Mariner"});
-        goToStats = new JButton("Go To Stats");
-        previous = new JButton("Previous");
-
-        setLayout(new BorderLayout());
-
-        this.add(createTopPanel(), BorderLayout.NORTH);
-        this.add(createCenterPanel(), BorderLayout.CENTER);
-        this.add(createBottomPanel(), BorderLayout.SOUTH);
-    }
-
-    private JPanel createTopPanel() {
-        JPanel titlePanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
-        titlePanel.add(title);
-        JPanel dropDownPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
-        dropDownPanel.add(chooseCultureDropdown);
-
-        JPanel borderPanel = new JPanel(new BorderLayout());
-        borderPanel.add(titlePanel, BorderLayout.NORTH);
-        borderPanel.add(dropDownPanel, BorderLayout.SOUTH);
-        return borderPanel;
-    }
-
-    private JSplitPane createCenterPanel() {
-        JSplitPane centerPanel = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, createDescriptionPane(), createSkillListPane());
-        centerPanel.setResizeWeight(0.5);
-        centerPanel.setContinuousLayout(true);
-        return centerPanel;
-    }
-
-    private JPanel createSkillListPane() {
-        JPanel panel = new JPanel(new BorderLayout());
+        String[] availableCultures = cultures.keySet().toArray(new String[0]);
         String[] columnNames = {"Skill Name", "Skill Points"};
         String[][] data = {
                 {"Perception", "2"},
@@ -90,6 +47,44 @@ public class ChooseCultureView extends JPanel {
                 {"Social Awareness", "3"},
                 {"Trading", "1"}
         };
+        //Initialize all components
+        title = new JLabel("It's time to choose your Culture");
+        Font font = new Font(title.getFont().getName(), Font.PLAIN, 16);
+        title.setFont(font);
+
+        chooseCultureDropdown = new JComboBox<>(availableCultures);
+        continueButton = new JButton("Go To Stats");
+        previousButton = new JButton("Previous");
+
+        setLayout(new BorderLayout());
+
+        this.add(createTopPanel(), BorderLayout.NORTH);
+        this.add(createCenterPanel(data, columnNames), BorderLayout.CENTER);
+        this.add(createBottomPanel(), BorderLayout.SOUTH);
+    }
+
+    private JPanel createTopPanel() {
+        JPanel titlePanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        titlePanel.add(title);
+        JPanel dropDownPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        dropDownPanel.add(chooseCultureDropdown);
+
+        JPanel borderPanel = new JPanel(new BorderLayout());
+        borderPanel.add(titlePanel, BorderLayout.NORTH);
+        borderPanel.add(dropDownPanel, BorderLayout.SOUTH);
+        return borderPanel;
+    }
+
+    private JSplitPane createCenterPanel(String[][] data, String[] columnNames) {
+        JSplitPane centerPanel = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, createDescriptionPane(), createSkillListPane(data, columnNames));
+        centerPanel.setResizeWeight(0.5);
+        centerPanel.setContinuousLayout(true);
+        return centerPanel;
+    }
+
+    private JPanel createSkillListPane(String[][] data, String[] columnNames) {
+        JPanel panel = new JPanel(new BorderLayout());
+
         JTable skillTable = new JTable(data, columnNames);
         skillTable.setAutoResizeMode(JTable.AUTO_RESIZE_ALL_COLUMNS);
         skillTable.setFillsViewportHeight(true);
@@ -98,35 +93,6 @@ public class ChooseCultureView extends JPanel {
 
         return panel;
     }
-
-    /*private JPanel createSkillListPane() {
-        JPanel panel = new JPanel();
-        GroupLayout layout = new GroupLayout(panel);
-        panel.setLayout(layout);
-        layout.setAutoCreateGaps(true);
-        layout.setAutoCreateContainerGaps(true);
-
-        GroupLayout.SequentialGroup hGroup = layout.createSequentialGroup();
-        GroupLayout.ParallelGroup labelGroup = layout.createParallelGroup(GroupLayout.Alignment.LEADING);
-        GroupLayout.ParallelGroup textFieldGroup = layout.createParallelGroup(GroupLayout.Alignment.LEADING);
-
-        for (int i = 0; i < labels.size(); i++) {
-            labelGroup.addComponent(labels.get(i));
-            textFieldGroup.addComponent(field.get(i)[0]);
-        }
-        hGroup.addGroup(labelGroup).addGroup(textFieldGroup);
-        layout.setHorizontalGroup(hGroup);
-
-        GroupLayout.SequentialGroup vGroup = layout.createSequentialGroup();
-
-        for(int i = 0; i < labels.size(); i++){
-            vGroup.addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
-                    .addComponent(labels.get(i))
-                    .addComponent(field.get(i)[0]));
-        }
-        layout.setVerticalGroup(vGroup);
-        return panel;
-    }*/
 
     private JPanel createDescriptionPane() {
         description = new JTextArea("COSMOPOLITAN:\n" +
@@ -156,44 +122,25 @@ public class ChooseCultureView extends JPanel {
 
     private JPanel createBottomPanel() {
         JPanel panel = new JPanel(new FlowLayout(FlowLayout.CENTER));
-        panel.add(previous);
-        panel.add(goToStats);
+        panel.add(previousButton);
+        panel.add(continueButton);
         return panel;
     }
 
-    /*public int getAnimalHandling() {
-        return getStat(0);
+    // Getters
+
+    public Culture getChosenCulture() {
+        return cultures.get(chooseCultureDropdown.getSelectedItem());
     }
-    public int getRiding() {
-        return getStat(1);
-    }
-    public int getUnarmed() {
-        return getStat(2);
-    }
-    public int getMeleeWeapons() {
-        return getStat(3);
-    }
-    public int getRangedWeapons() {
-        return getStat(4);
-    }
-    public int getLanguages() {
-        return getStat(5);
-    }
-    public int getOtherLores() {
-        return getStat(6);
-    }
-    public int getInfluence() {
-        return getStat(7);
-    }*/
 
     //Action Listeners
 
     public void previousButtonClick (ActionListener actionListener) {
-        previous.addActionListener(actionListener);
+        previousButton.addActionListener(actionListener);
     }
 
-    public void goToStatsButtonClick (ActionListener actionListener) {
-        goToStats.addActionListener(actionListener);
+    public void continueButtonClick(ActionListener actionListener) {
+        continueButton.addActionListener(actionListener);
     }
 
     public void reset(){
