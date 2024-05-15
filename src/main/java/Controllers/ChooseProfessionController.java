559b293007117a794c3f62dfe977a5cedac04f94
AndyTheDragon <andre.jansam@gmail.com>
2024-05-16 00:05:53 +0200

    Code reformat

--- src/main/java/Controllers/ChooseProfessionController.java
+++ src/main/java/Controllers/ChooseProfessionController.java
@@ -14,22 +14,24 @@ public class ChooseProfessionController {
         this.view = view;
 
     }
+
     private boolean validate() {
         return view.isProfessionSelected();
 
     }
+
     private void saveDataToModel(Profession profession) {
         model.setProfession(profession);
 
     }
 
-    public boolean validateAndSave(){
-    Profession profession = view.getSelectedProfession();
-    if(validate()){
-        saveDataToModel(profession);
-        System.out.println("Selected Profession: " + profession.getName());
-        return true;
-    }
-    return false;
+    public boolean validateAndSave() {
+        Profession profession = view.getSelectedProfession();
+        if (validate()) {
+            saveDataToModel(profession);
+            System.out.println("Selected Profession: " + profession.getName());
+            return true;
+        }
+        return false;
     }
 }
