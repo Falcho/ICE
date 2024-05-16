841d1586a27a85eef9bba3d415db88bd03296444
MojoOno <Ffranck0406@gmail.com>
2024-05-16 10:57:37 +0200

    Added version 1 of RaceView

---
+++ src/main/java/Views/ChooseRaceView.java
@@ -0,0 +1,172 @@
+package Views;
+
+import Models.Race;
+
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.awt.*;
+import java.util.Map;
+
+public class ChooseRaceView extends JPanel {
+    private final String[] columnNames;
+    private final String[][] data;
+    private Map<String, Race> races;
+    private JLabel title;
+    private JLabel sizeLabel;
+    private JLabel statBonusLabel;
+    private JLabel resistanceLabel;
+    private JLabel healthAndDevLabel;
+    private JComboBox<String> dropDown;
+    private JTextArea description;
+    private JLabel tAndFlawTitle;
+    private JTextArea tAndFlawDescription;
+    private JTable sizeTable;
+    private JTable statBonusTable;
+    private JTable resistanceTable;
+    private JTable healthAndDevTable;
+    private JButton previousButton;
+    private JButton continueButton;
+    DefaultTableModel sizeTableModel;
+    DefaultTableModel statBonusTableModel;
+    DefaultTableModel resistanceTableModel;
+    DefaultTableModel healthAndDevTableModel;
+
+    //Map<String, Race> races ---- skal ind i constructor som argument
+    public ChooseRaceView() {
+        //this.races = races;
+
+        //String[] availableRaces = races.keySet().toArray(new String[0]);
+        columnNames = new String[]{"Name", "Skill Points", "Name", "Skill Points"};
+        data = new String [][]{
+                {"Skill", "1", "Skill", "1"},
+                {"Skill", "1", "Skill", "1"},
+                {"Skill", "1", "Skill", "1"},
+                {"Skill", "1", "Skill", "1"},
+                {"Skill", "1", "Skill", "1"},
+                {"NEW TABLE UNDER", "1", "NEW TABLE UNDER", "1"},
+        };
+
+        //Initialize all Components
+        title = new JLabel("It's time to choose your Race");
+        Font font = new Font(title.getFont().getName(), Font.PLAIN, 16);
+        title.setFont(font);
+        tAndFlawTitle = new JLabel("Racial Talents & Flaws");
+        tAndFlawDescription = new JTextArea("DESCRIPTION");
+        sizeLabel = new JLabel();
+        statBonusLabel = new JLabel();
+        resistanceLabel = new JLabel();
+        healthAndDevLabel = new JLabel();
+        sizeTable = new JTable();
+        statBonusTable = new JTable();
+        resistanceTable = new JTable();
+        healthAndDevTable = new JTable();
+
+        dropDown = new JComboBox<>();
+        previousButton = new JButton("Previous");
+        continueButton = new JButton("Continue to Race");
+
+        setLayout(new BorderLayout());
+
+        this.add(createTopPane(), BorderLayout.NORTH);
+        this.add(createCenterSplitPane(), BorderLayout.CENTER);
+        this.add(createBottomPane(), BorderLayout.SOUTH);
+
+
+
+    }
+
+    private JPanel createTopPane(){
+        JPanel titlePanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        titlePanel.add(title);
+        JPanel dropDownPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        dropDownPanel.add(dropDown);
+
+        JPanel borderPanel = new JPanel(new BorderLayout());
+        borderPanel.add(titlePanel, BorderLayout.NORTH);
+        borderPanel.add(dropDownPanel, BorderLayout.SOUTH);
+
+        return borderPanel;
+    }
+
+    private JSplitPane createCenterSplitPane(){
+        JSplitPane centerPanel = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, createDescriptionPane(), createRightOuterPane());
+        centerPanel.setResizeWeight(0.5);
+        centerPanel.setContinuousLayout(true);
+        return centerPanel;
+    }
+
+    private JPanel createDescriptionPane(){
+        description = new JTextArea("Choose a race from the dropdown, Choose a race from the dropdown, Choose a race from the dropdown, " +
+                "Choose a race from the dropdown, Choose a race from the dropdown, Choose a race from the dropdown, " +
+                "Choose a race from the dropdown, Choose a race from the dropdown, Choose a race from the dropdown, " +
+                "Choose a race from the dropdown, Choose a race from the dropdown, Choose a race from the dropdown, " +
+                "Choose a race from the dropdown, Choose a race from the dropdown, Choose a race from the dropdown, ");
+        description.setEditable(false);
+        description.setLineWrap(true);
+        description.setWrapStyleWord(true);
+        description.setBorder(BorderFactory.createEmptyBorder(20,20,20,20));
+
+
+        JPanel panel = new JPanel(new BorderLayout());
+        panel.add(description,BorderLayout.NORTH);
+
+        return panel;
+    }
+
+    private JPanel createRightOuterPane(){
+        JPanel borderPanel = new JPanel(new BorderLayout());
+
+        JPanel talentAndFlawPanel = new JPanel(new BorderLayout());
+        talentAndFlawPanel.add(tAndFlawTitle, BorderLayout.NORTH);
+        talentAndFlawPanel.add(tAndFlawDescription, BorderLayout.SOUTH);
+
+        JPanel racialOuterPanel = new JPanel(new BorderLayout());
+        JPanel racialTopInner = new JPanel(new BorderLayout());
+        //sizeLabel = new JLabel("Race Size");
+        sizeTableModel = new DefaultTableModel(this.data, this.columnNames);
+        sizeTable = new JTable(sizeTableModel);
+        //statBonusLabel = new JLabel("Stat Bonus");
+        statBonusTableModel = new DefaultTableModel(this.data, this.columnNames);
+        statBonusTable = new JTable(statBonusTableModel);
+        //racialTopInner.add(sizeLabel,BorderLayout.LINE_START);
+        //racialTopInner.add(statBonusLabel,BorderLayout.LINE_END);
+        racialTopInner.add(sizeTable, BorderLayout.WEST);
+        racialTopInner.add(statBonusTable, BorderLayout.EAST);
+
+        JPanel racialBottomInner = new JPanel(new BorderLayout());
+        //resistanceLabel = new JLabel("Resistances");
+        resistanceTableModel = new DefaultTableModel(this.data, this.columnNames);
+        resistanceTable = new JTable(resistanceTableModel);
+        //healthAndDevLabel = new JLabel("Health and Dev");
+        healthAndDevTableModel = new DefaultTableModel(this.data, this.columnNames);
+        healthAndDevTable = new JTable(healthAndDevTableModel);
+        //racialBottomInner.add(resistanceLabel,BorderLayout.LINE_START);
+        //racialBottomInner.add(healthAndDevLabel,BorderLayout.LINE_END);
+        racialBottomInner.add(resistanceTable, BorderLayout.WEST);
+        racialBottomInner.add(healthAndDevTable, BorderLayout.EAST);
+
+        racialOuterPanel.add(racialTopInner, BorderLayout.NORTH);
+        racialOuterPanel.add(racialBottomInner, BorderLayout.SOUTH);
+
+
+        JSplitPane splitTalentAndRacial = new JSplitPane(JSplitPane.VERTICAL_SPLIT, talentAndFlawPanel, racialOuterPanel);
+        splitTalentAndRacial.setResizeWeight(0.5);
+        splitTalentAndRacial.setContinuousLayout(true);
+
+        borderPanel.add(talentAndFlawPanel, BorderLayout.NORTH);
+        borderPanel.add(racialOuterPanel, BorderLayout.SOUTH);
+
+        return borderPanel;
+
+    }
+
+    private JPanel createBottomPane(){
+        //Buttons go here
+        JPanel panel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        panel.add(previousButton);
+        panel.add(continueButton);
+        return panel;
+    }
+
+
+}
